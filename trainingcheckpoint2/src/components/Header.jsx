ef1a88ceed35e1ec6e5df3c776e0fd54c5a5d07e
lisagaudinWCS <lisagaudin.wcs@gmail.com>
2022-07-26 10:32:41 +0200

    Header component with WelcomeMessage props.name

---
+++ trainingcheckpoint2/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header(props) {
+    return (
+      <div className="Header">
+        <h1>Welcome to our {props.name} App !</h1>
+      </div>
+    );
+  }
+export default Header;
