7d4c204a3b12feefc988a479728ad87ba371d964
lisagaudinWCS <lisagaudin.wcs@gmail.com>
2022-07-26 15:00:28 +0200

    filter button ok

--- trainingcheckpoint2/src/components/Game.jsx
+++ trainingcheckpoint2/src/components/Game.jsx
@@ -1,10 +1,11 @@
 import '../components/game.css'
-function Game ({name, image, date}) {
+function Game ({name, image, rating, date}) {
 
     return (
         <div className="game-container">
             <h3 className='game-name'>{name}</h3>
-            <h4 className='game-date'>{date}</h4>
+            <h4 className='game-date'>Released in {date}</h4>
+            <h5 className='game-rating'>Note : {rating}</h5>
             <img className='game-img' src={image} alt={name} height={150}/>
         </div>
     )
--- trainingcheckpoint2/src/components/GameList.jsx
+++ trainingcheckpoint2/src/components/GameList.jsx
@@ -2,8 +2,24 @@ import React, {useState, useEffect} from "react";
 import axios from "axios";
 import Game from "./Game";
 
+
+
+// 1/ useState que tu initialises avec un booléen pour pouvoir changer l'état par la suite
+// 2/creer une fonction qui va inverser le useState
+// 3/ un bouton qui lorsque onClick appelle la fonction ci-dessus
+// 3/.filter ( renvoi le tableau filtré  - donc soit tous les éléments soit > 4.5)
+// 4/.map pour afficher le resultat
+
+
     const GameList = () => {
        
+        const [games, setGames] = useState("");
+        const [filtered, setFiltered] = useState(false);
+
+        const handleSwitch = () => {
+            setFiltered(!filtered)
+        }
+
         const getGames = () => {
             axios
                 .get(`https://apis.wilders.dev/wild-games/games`)
@@ -13,11 +29,7 @@ import Game from "./Game";
                 .then((data) => {
                     setGames(data);
                 });
-                // Transmettre cette data pour mettre à jour le state
         };
-    
-        const [games, setGames] = useState("")
-        // Créer un state pour stocker les données reçues de l'API
        
         useEffect(() => getGames(), [games])
 
@@ -26,19 +38,21 @@ import Game from "./Game";
     return (
         <div className="GameList">
             <h2>GameList</h2>
+            <button onClick={handleSwitch}>Game Filter</button>
 
-            {games && 
-          games
-          .map((element, index) => (
-            <Game 
-            key={index}
-            name={element.name}
-            rating={element.rating}
-            date={element.released}
-            image={element.background_image}
-            />)
-            )
-          }
+            {games && games
+            .filter(value => !filtered || value.rating > 4.5)
+
+            .map((element, index) => (
+                <Game 
+                key={index}
+                name={element.name}
+                rating={element.rating}
+                date={element.released}
+                image={element.background_image}
+                />)
+                )
+            }
 
         </div>
     )
